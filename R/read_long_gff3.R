6b0f048582cb922dda7240a173597e7bddda6172
davidtong28 <tongshanwei111@gmail.com>
2023-09-18 15:30:53 +0800

    fixed input_data_long when short gff is given

--- R/read_long_gff3.R
+++ R/read_long_gff3.R
@@ -1,7 +1,7 @@
 #' Splits a long GFF3 file into two files and stores the annotation into the variable "gff_NAME" (which is `gff` by default) and FASTA into "concat_dna_NAME" (concatenated DNA, `dna` by default) and "dna_NAME" (DNA in contigs, `dna_contigs` by default).
 #'
 #' @param longgff_path Relative path to the long GFF3 file
-#' @param keepDNA Option to keep DNA. Defaults to TRUE.
+#' @param keepDNA Option to keep DNA. Defaults to TRUE.When FALSE, the provided GFF3 file will be regarded as a short GFF3 and only annotation data will be saved
 #' @param gff_NAME Variable name which stores the GFF annotation table. Defaults to "gff"
 #' @param concat_dna_NAME Variable name which stores the concatenated FASTA. Defaults to "dna"
 #' @param dna_NAME Variable name which stores the fragmented FASTA. Defaults to "dna_contigs" (or concat_dna_NAME+"_contigs")
@@ -13,10 +13,11 @@
 #' @export
 
 read_long_gff3 <- function(longgff_path,keepDNA=T,gff_NAME="gff",concat_dna_NAME="dna",dna_NAME=paste(concat_dna_NAME,"_contigs",sep = "")){
-  shell(paste("csplit ",longgff_path," /FASTA/"))
-  shortgff3<-read.gff("xx00",GFF3=T)  
-  assign(gff_NAME,value = shortgff3,envir = .GlobalEnv)
+
   if(keepDNA){
+    shell(paste("csplit ",longgff_path," /FASTA/"))
+    shortgff3<-read.gff("xx00",GFF3=T)  
+    assign(gff_NAME,value = shortgff3,envir = .GlobalEnv)
     shell('tail -n +2 xx01>xx001')
     gffdna<-readDNAStringSet("xx001")
     assign(dna_NAME,value = gffdna,envir = .GlobalEnv)
@@ -25,6 +26,9 @@ read_long_gff3 <- function(longgff_path,keepDNA=T,gff_NAME="gff",concat_dna_NAME
       paste0(collapse = "") %>% 
       DNAString()
     assign(concat_dna_NAME,value = DNA,envir = .GlobalEnv)
+  }else{
+    shortgff3<-read.gff("xx00",GFF3=T)  
+    assign(gff_NAME,value = shortgff3,envir = .GlobalEnv)
   }
 }
 
